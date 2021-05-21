5d097f06d7e66b583a2cee11c7164aeaf3e51667
Dan Powell <powell956@gmail.com>
2021-05-21 15:51:23 -0400

    commit tree fd079451ff42b9e93f5e8add4912e18556a1051b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+
+doc.css(".headline-26oIBN")
+
+
+courses = doc.css("2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2YO002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+    puts course.text.strip
+end
